b9324d6123f732fa98c98e1c9752e8f0ea7b9e21
Ben Freke <benfreke@gmail.com>
2015-03-15 17:40:40 +1000

    Merged all changes

--- controllers/Menus.php
+++ controllers/Menus.php
@@ -49,7 +49,6 @@ class Menus extends Controller
         unset($new_post_data['Menu']['internal_url']);
         unset($new_post_data['Menu']['external_url']);
         Input::replace($new_post_data);
-        $_POST = $new_post_data;
     }
 
     /**
