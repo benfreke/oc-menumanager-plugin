benfreke/oc-menumanager-plugin

commit 8bccc823cedc550106c3c6299850692e9fd877e1

Reformatting of files

models/Menu.php
@@ -1,12 +1,12 @@
 <?php namespace BenFreke\MenuManager\Models;
 
-use Model;
-use Lang;
-use Validator;
+use Cms\Classes\Controller as BaseController;
 use Cms\Classes\Page;
 use Cms\Classes\Theme;
-use Cms\Classes\Controller as BaseController;
+use Lang;
+use Model;
 use System\Classes\ApplicationException;
+use Validator;
 
 /**
  * Menu Model
@@ -18,11 +18,6 @@ class Menu extends Model
     use \October\Rain\Database\Traits\Purgeable;
 
     /**
-     * @var \Cms\Classes\Controller A reference to the CMS controller.
-     */
-    private $controller;
-
-    /**
      * @var string The database table used by the model.
      */
     public $table = 'benfreke_menumanager_menus';
@@ -31,6 +26,14 @@ class Menu extends Model
      * @var array Translatable fields
      */
     public $translatable = ['title', 'description'];
+
+    /**
+     * @var array Validation rules
+     */
+    public $rules = [
+        'title'      => 'required',
+        'parameters' => 'json'
+    ];
 
     /**
      * @var array Guarded fields
@@ -48,15 +51,13 @@ class Menu extends Model
     protected $purgeable = ['internal_url', 'external_url'];
 
     /**
-     * @var array Validation rules
+     * @var \Cms\Classes\Controller A reference to the CMS controller.
      */
-    public $rules = [
-        'title'      => 'required',
-        'parameters' => 'json'
-    ];
+    private $controller;
 
     /**
      * Add translation support to this model, if available.
+     *
      * @return void
      */
     public static function boot()
@@ -65,6 +66,7 @@ class Menu extends Model
             'json',
             function ($attribute, $value, $parameters) {
                 json_decode($value);
+
                 return json_last_error() == JSON_ERROR_NONE;
             }
         );
@@ -83,23 +85,24 @@ class Menu extends Model
 
                 // Implement the translatable behavior
                 $model->implement[] = 'RainLab.Translate.Behaviors.TranslatableModel';
-
             }
         );
     }
 
     /**
      * Returns the list of menu items, where the key is the id and the value is the title, indented with '-' for depth
+     *
      * @return array
      */
     public function getSelectList()
     {
-        $items  = $this->getAll();
+        $items = $this->getAll();
         $output = array();
         foreach ($items as $item) {
-            $depthIndicator         = $this->getDepthIndicators($item->nest_depth);
+            $depthIndicator = $this->getDepthIndicators($item->nest_depth);
             $output["id-$item->id"] = $depthIndicator . ' ' . $item->title;
         }
+
         return $output;
     }
 
@@ -116,6 +119,7 @@ class Menu extends Model
         if ($depth < 1) {
             return $indicators;
         }
+
         return $this->getDepthIndicators(--$depth, $indicators . '-');
     }
 
@@ -127,23 +131,14 @@ class Menu extends Model
     public function getUrlOptions()
     {
         $allPages = Page::sortBy('baseFileName')->lists('title', 'baseFileName');
-        $pages    = array(
+        $pages = array(
             '' => Lang::get('benfreke.menumanager::lang.create.nolink')
         );
         foreach ($allPages as $key => $value) {
             $pages[$key] = "{$value} - (File: $key)";
         }
-        return $pages;
-    }
 
-    /**
-     * Returns the class name so I can compare
-     *
-     * @return string
-     */
-    public static function getClassName()
-    {
-        return get_called_class();
+        return $pages;
     }
 
     /**
@@ -171,9 +166,9 @@ class Menu extends Model
         }
 
         if (!empty($this->query_string)) {
-            if (substr($this->query_string,0,1)=='#'){
+            if (substr($this->query_string, 0, 1) == '#') {
                 $url .= $this->query_string;
-            }else{
+            } else {
                 $url .= '?' . $this->createQueryString($this->query_string);
             }
         }
@@ -215,7 +210,7 @@ class Menu extends Model
     /**
      * Load the item classes here to keep the twig template clean
      *
-     * @param int $leftIndex  The left value of the active node
+     * @param int $leftIndex The left value of the active node
      * @param int $rightIndex The right value of the active node
      *
      * @return string

components/Menu.php
@@ -1,11 +1,11 @@
 <?php namespace BenFreke\MenuManager\Components;
 
-use Cms\Classes\ComponentBase;
-use BenFreke\MenuManager\Models\Menu as menuModel;
-use Request;
 use App;
+use BenFreke\MenuManager\Models\Menu as menuModel;
+use Cms\Classes\ComponentBase;
 use DB;
 use Lang;
+use Request;
 
 class Menu extends ComponentBase
 {
@@ -72,17 +72,6 @@ class Menu extends ComponentBase
                 ]
             ]
         ];
-
-    }
-
-    /**
-     * Returns the list of menu items I can select
-     * @return array
-     */
-    public function getStartOptions()
-    {
-        $menuModel = new menuModel();
-        return $menuModel->getSelectList();
     }
 
     /**
@@ -99,25 +88,37 @@ class Menu extends ComponentBase
     }
 
     /**
+     * Returns the list of menu items I can select
+     *
+     * @return array
+     */
+    public function getStartOptions()
+    {
+        $menuModel = new menuModel();
+
+        return $menuModel->getSelectList();
+    }
+
+    /**
      * Build all my parameters for the view
+     *
      * @todo Pull as much as possible into the model, including the column names
      */
     public function onRender()
     {
         // Set the parentNode for the component output
-        $topNode                  = menuModel::find($this->getIdFromProperty($this->property('start')));
+        $topNode = menuModel::find($this->getIdFromProperty($this->property('start')));
         $this->page['parentNode'] = $topNode;
 
         // What page is active?
-        $this->page['activeLeft']  = 0;
+        $this->page['activeLeft'] = 0;
         $this->page['activeRight'] = 0;
-        $activeNode                = $this->getIdFromProperty($this->property('activeNode'));
+        $activeNode = $this->getIdFromProperty($this->property('activeNode'));
 
         if ($activeNode) {
 
             // It's been set by the user, so use what they've set it as
             $activeNode = menuModel::find($activeNode);
-
         } elseif ($topNode) {
 
             // Go and find the page we're on
@@ -136,7 +137,7 @@ class Menu extends ComponentBase
 
         // If I've got a result that is a node
         if ($activeNode && menuModel::getClassName() === get_class($activeNode)) {
-            $this->page['activeLeft']  = (int)$activeNode->nest_left;
+            $this->page['activeLeft'] = (int)$activeNode->nest_left;
             $this->page['activeRight'] = (int)$activeNode->nest_right;
         }
 
@@ -144,10 +145,10 @@ class Menu extends ComponentBase
         $this->page['numberOfLevels'] = (int)$this->property('numberOfLevels');
 
         // Add the classes to the view
-        $this->page['primaryClasses']   = $this->property('primaryClasses');
+        $this->page['primaryClasses'] = $this->property('primaryClasses');
         $this->page['secondaryClasses'] = $this->property('secondaryClasses');
-        $this->page['tertiaryClasses']  = $this->property('tertiaryClasses');
-        $this->page['listItemClasses']  = $this->property('listItemClasses');
+        $this->page['tertiaryClasses'] = $this->property('tertiaryClasses');
+        $this->page['listItemClasses'] = $this->property('listItemClasses');
     }
 
     /**
@@ -164,6 +165,7 @@ class Menu extends ComponentBase
         if (!strlen($value) > 3) {
             return false;
         }
+
         return substr($value, 3);
     }
 

controllers/Menus.php
@@ -106,7 +106,7 @@ class Menus extends Controller
 
         $this->pageTitle = Lang::get('benfreke.menumanager::lang.menu.reordermenu');
 
-        $toolbarConfig          = $this->makeConfig();
+        $toolbarConfig = $this->makeConfig();
         $toolbarConfig->buttons = '@/plugins/benfreke/menumanager/controllers/menus/_reorder_toolbar.htm';
 
         $this->vars['toolbar'] = $this->makeWidget('Backend\Widgets\Toolbar', $toolbarConfig);

updates/fix_menu_table.php
@@ -8,14 +8,14 @@ class FixMenuTable extends Migration
 
     public function up()
     {
-//        DB::statement('ALTER TABLE `benfreke_menumanager_menus` MODIFY `nest_left` INTEGER UNSIGNED NULL;');
-//        DB::statement('ALTER TABLE `benfreke_menumanager_menus` MODIFY `nest_right` INTEGER UNSIGNED NULL;');
+        DB::statement('ALTER TABLE `benfreke_menumanager_menus` MODIFY `nest_left` INTEGER UNSIGNED NULL;');
+        DB::statement('ALTER TABLE `benfreke_menumanager_menus` MODIFY `nest_right` INTEGER UNSIGNED NULL;');
     }
 
     public function down()
     {
-//        DB::statement('ALTER TABLE `benfreke_menumanager_menus` MODIFY `nest_left` INTEGER UNSIGNED;');
-//        DB::statement('ALTER TABLE `benfreke_menumanager_menus` MODIFY `nest_right` INTEGER UNSIGNED;');
+        DB::statement('ALTER TABLE `benfreke_menumanager_menus` MODIFY `nest_left` INTEGER UNSIGNED;');
+        DB::statement('ALTER TABLE `benfreke_menumanager_menus` MODIFY `nest_right` INTEGER UNSIGNED;');
     }
 
 }

commit 48165d2b7679d6efaa29f4b766adf7d6a1483359

Fixed an issue that #8bccc823 introduced

models/Menu.php
@@ -142,6 +142,16 @@ class Menu extends Model
     }
 
     /**
+     * Returns the class name so I can compare
+     *
+     * @return string
+     */
+    public static function getClassName()
+    {
+        return get_called_class();
+     }
+
+    /**
      * Returns the correct url for this menu item.
      * It will either be the full page URL or '#' if no link was provided
      *